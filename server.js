3f7c10097a69fc40b31370ada12f1509960a0a90
habahram <hamid.bahramian@gmail.com>
2020-03-29 21:23:49 -0400

    .

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-//create the server 
+//create the server
 const app = express();
 const port = process.env.PORT || 4003;
 
@@ -8,3 +8,6 @@ const port = process.env.PORT || 4003;
 app.get('/', (request, response) => {
   response.sendFile(__dirname + '/index.html');
 });
+
+// start the server
+app.listen(port, () => console.log('Listening on port ' + port));
