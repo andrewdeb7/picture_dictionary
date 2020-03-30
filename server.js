335eb1598e79c12110b9876908c02efcbc4ac597
habahram <hamid.bahramian@gmail.com>
2020-03-29 23:37:55 -0400

    .

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', (request, response) => {
   response.sendFile(__dirname + '/index.html');
 });
 
+app.get('/index.html', (request, response) => {
+  response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/pages.html', (request, response) => {
+  response.sendFile(__dirname + '/pages.html');
+});
+
 app.get('/admin.html', (request, response) => {
   response.sendFile(__dirname + '/admin.html');
 });
